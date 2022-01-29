c8d07eca44df2d936387f89b084d6b5083b992ad
jaieve-ps <yoori0303@naver.com>
2022-01-29 13:20:08 +0900

    Toggle Switch 메시지 추가

--- custom-component/src/component/Toggle.js
+++ custom-component/src/component/Toggle.js
@@ -10,14 +10,13 @@ function Toggle(props) {
     return (
         <div className="row h-25">
             <h3 className="text-start">Toggle</h3>
-            <div className="d-flex justify-content-center">
+            <div className="d-flex justify-content-center py-auto my-0">
                 <div className={`toggle-container ${selected ? "" : "disabled"}`}
                      onClick={toggleHandler}>
-                    <div className={`dialog-button`}>
-                        {/*{selected ? "YES" : "NO"}*/}
-                    </div>
+                    <div className={`dialog-button`}/>
                 </div>
             </div>
+            <span>Toggle Switch {selected ? "ON" : "OFF"}</span>
         </div>
     );
 }
