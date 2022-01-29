8566adea8974b22d3bea5d9ed3166b00873883df
jaieve-ps <yoori0303@naver.com>
2022-01-29 13:16:10 +0900

    Toggle 완성

--- custom-component/src/component/Toggle.js
+++ custom-component/src/component/Toggle.js
@@ -1,27 +1,26 @@
 import React, {useState} from 'react';
-import styled from "styled-components";
+import "../../src/style/toggleStyle.css";
 
 function Toggle(props) {
-    // const [ToggleOn, setToggleOn] = useState(false);
-    //
-    // this.handleChange = this.handleChange.bind(this);
-    //
-    // const clickHandler = () => {
-    //     setToggleOn(prevState => ({}))
-    // }
-    const toggleBtn = styled.div`
-      grid-column: 4;
-      grid-row: 1/2;
-    `;
+    const [selected, setSelected] = useState(false);
 
+    const toggleHandler = () => {
+        setSelected(!selected);
+    }
     return (
         <div className="row h-25">
             <h3 className="text-start">Toggle</h3>
-            <div className="row justify-content-center">
-                Toggle
+            <div className="d-flex justify-content-center">
+                <div className={`toggle-container ${selected ? "" : "disabled"}`}
+                     onClick={toggleHandler}>
+                    <div className={`dialog-button`}>
+                        {/*{selected ? "YES" : "NO"}*/}
+                    </div>
+                </div>
             </div>
         </div>
     );
 }
+
 
 export default Toggle;
