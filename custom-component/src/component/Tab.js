1a7552b0cb3366bc36e9023979c08458046860e9
jaieve-ps <yoori0303@naver.com>
2022-01-30 14:07:46 +0900

    Tab 완성

--- custom-component/src/component/Tab.js
+++ custom-component/src/component/Tab.js
@@ -1,27 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
+import ComponentName from "./ComponentName";
+import "../../src/style/tabStyle.css";
+
 
 
 function Tab(props) {
-    const handleClick = (e) => {
-        const className = e.target.className;
-        const id = e.target.id;
-        console.log(className, id);
-        const tabs = document.getElementsByClassName('tabMenu');
-        console.log(tabs);
-        // 클릭한 tab에 clicked이 없으면
-        // 모든 tabMenu에서 clicked 제거
-        // 클릭된 tab에만 className에 clicked 추가
+    const [tab, setTab ] =useState(0);
+    const handleClick = (id) => {
+        console.log(id);
+        setTab(id);
     }
     const clicked = false;
     return (
         <div className="row h-25">
-            <h3 className="text-start">Tab</h3>
+            <ComponentName componentsName="Tab"/>
             <div className="row" id="tab" style={{background: 'lightgray', height: '50px'}}>
-                <div className="col-1" id="logo">null</div>
-                <div className={`col-3 tabMenu ${clicked ? "clicked" : ""}`} id="tab1" onClick={handleClick}>tab1</div>
-                <div className={`col-3 tabMenu ${clicked ? "clicked" : ""}`} id="tab2" onClick={handleClick}>tab2</div>
-                <div className={`col-3 tabMenu ${clicked ? "clicked" : ""}`} id="tab3" onClick={handleClick}>tab3</div>
+                <div className={`col-4 px-0 mx-0 tabMenu ${tab===0 ? "clicked" : ""}`} id="tab1" onClick={() => {handleClick(0)}}>tab1</div>
+                <div className={`col-4 px-0 mx-0 tabMenu ${tab===1 ? "clicked" : ""}`} id="tab2" onClick={() => {handleClick(1)}}>tab2</div>
+                <div className={`col-4 px-0 mx-0 tabMenu ${tab===2 ? "clicked" : ""}`} id="tab3" onClick={() => {handleClick(2)}}>tab3</div>
             </div>
+            <span>Tab Menu {tab + 1}</span>
         </div>
     );
 }
